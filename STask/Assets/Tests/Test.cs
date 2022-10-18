7332d9d351e116a7b536f47a599b6f50b7230a11
Summid <Summid@foxmail.com>
2022-10-18 23:38:05 +0800

    测试MinimumQueue代码

--- STask/Assets/Tests/Test.cs
+++ STask/Assets/Tests/Test.cs
@@ -1,12 +1,41 @@
+using SFramework.Threading.Tasks.Internal;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        
     }
+    #region 测试MinimumQueue
+    //private MinimumQueue<int> minimumQueue = new MinimumQueue<int>(0);
+    //private int version = 0;
+    //private Vector2 scrollPos = Vector2.zero;
+    //private void OnGUI()
+    //{
+    //    GUILayout.BeginHorizontal();
+    //    {
+    //        if (GUI.Button(GUILayoutUtility.GetRect(100, 50), "入队"))
+    //        {
+    //            this.minimumQueue.Enqueue(version++);
+    //        }
+    //        if (GUI.Button(GUILayoutUtility.GetRect(100, 50), "出队"))
+    //        {
+    //            this.minimumQueue.Dequeue();
+    //        }
+    //    }
+    //    GUILayout.EndHorizontal();
+
+    //    this.scrollPos = GUILayout.BeginScrollView(this.scrollPos);
+    //    {
+    //        foreach (var item in minimumQueue.array)
+    //        {
+    //            GUILayout.Label(new GUIContent(item.ToString()));
+    //        }
+    //    }
+    //    GUILayout.EndScrollView();
+    //}
+    #endregion
 }
