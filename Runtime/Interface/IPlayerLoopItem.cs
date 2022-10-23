ca020f7eb2c35026427d60ef6923e510d23bf754
Summid <Summid@foxmail.com>
2022-10-23 17:28:57 +0800

    添加IPlayerLoopItem接口

---
+++ Runtime/Interface/IPlayerLoopItem.cs
@@ -0,0 +1,13 @@
+namespace SFramework.Threading.Tasks
+{
+    /// <summary>
+    /// PlayerLoopSystem的迭代对象
+    /// 提供<see cref="MoveNext"/>方法供PlayerLoopSystem迭代
+    /// （类似IEnumerator）
+    /// </summary>
+    /// <seealso href="https://learn.microsoft.com/zh-cn/dotnet/api/system.collections.ienumerator"/>IEnumerator参考
+    public interface IPlayerLoopItem
+    {
+        bool MoveNext();
+    }
+}
