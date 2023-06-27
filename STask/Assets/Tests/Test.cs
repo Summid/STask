d476b1d1b6a1816526261499e34c9481821a1ea6
Summid <summid@foxmail.com>
2023-06-27 10:25:00 +0800

    add STask.WhenAll<T>

--- STask/Assets/Tests/Test.cs
+++ STask/Assets/Tests/Test.cs
@@ -7,10 +7,6 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private void Start()
-    {
-    }
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
@@ -21,68 +17,21 @@ public class Test : MonoBehaviour
 
     public async void TestWhenAll()
     {
-        var task = STask.Delay(5000);
-        var task2 = STask.Delay(1000);
-        Debug.Log("start await all task");
-        await STask.WhenAll(task, task2);
-        Debug.Log("all tasks completed");
+        Debug.LogWarning("Test Start");
+        var task1 = this.AsyncMethod(100);
+        var task2 = this.AsyncMethod(233);
+        var task3 = this.AsyncMethod(777);
+        var awaitResult = await STask.WhenAll(task1, task2, task3);
+        foreach (var result in awaitResult)
+        {
+            Debug.Log($"time {result}");
+        }
+        Debug.LogWarning("Test End");
     }
-    
 
-    public class TestTaskPoolNode : ITaskPoolNode<TestTaskPoolNode>
+    public async STask<float> AsyncMethod(int milliseconds)
     {
-        private static TaskPool<TestTaskPoolNode> pool;
-
-        private TestTaskPoolNode nextNode;
-        public ref TestTaskPoolNode NextNode => ref this.nextNode;
-
-        static TestTaskPoolNode()
-        {
-            TaskPool.RegisterSizeGetter(typeof(TestTaskPoolNode), () => pool.Size);
-        }
-
-        public static TestTaskPoolNode Create()
-        {
-            if (pool.TryPop(out var node))
-            {
-                node = new TestTaskPoolNode();
-            }
-            return node;
-        }
-
-        public bool Dispose()
-        {
-            return pool.TryPush(this);
-        }
+        await STask.Delay(milliseconds);
+        return Time.realtimeSinceStartup;
     }
-    
-    #region 测试MinimumQueue
-    //private MinimumQueue<int> minimumQueue = new MinimumQueue<int>(0);
-    //private int version = 0;
-    //private Vector2 scrollPos = Vector2.zero;
-    //private void OnGUI()
-    //{
-    //    GUILayout.BeginHorizontal();
-    //    {
-    //        if (GUI.Button(GUILayoutUtility.GetRect(100, 50), "入队"))
-    //        {
-    //            this.minimumQueue.Enqueue(version++);
-    //        }
-    //        if (GUI.Button(GUILayoutUtility.GetRect(100, 50), "出队"))
-    //        {
-    //            this.minimumQueue.Dequeue();
-    //        }
-    //    }
-    //    GUILayout.EndHorizontal();
-
-    //    this.scrollPos = GUILayout.BeginScrollView(this.scrollPos);
-    //    {
-    //        foreach (var item in minimumQueue.array)
-    //        {
-    //            GUILayout.Label(new GUIContent(item.ToString()));
-    //        }
-    //    }
-    //    GUILayout.EndScrollView();
-    //}
-    #endregion
 }
