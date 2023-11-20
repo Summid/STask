a394e449d3ee9795920c2f73e09fd96903b2fb07
Summid <summid@foxmail.com>
2023-11-20 18:29:36 +0800

    add Editor asmdef and SplitterGUILayout

---
+++ STask/Assets/Plugins/STask/Editor/STaskTrackerWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEditor;
+
+namespace SFramework.Threading.Tasks.Editor
+{
+    public class STaskTrackerWindow : EditorWindow
+    {
+        private static STaskTrackerWindow window;
+
+        [MenuItem("Window/STask Tracker")]
+        public static void OpenWindow()
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+            
+            // will called OnEnable(singleton instance will be set).
+            GetWindow<STaskTrackerWindow>("STask Tracker").Show();
+        }
+
+        private object splitterState;
+
+        private void OnEnable()
+        {
+            window = this;// set singleton.
+            this.splitterState = SplitterGUILayout.CreateSplitterState(new float[] { 75f, 25f }, new int[] { 32, 32 }, null);
+        }
+    }
+}
