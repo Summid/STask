a1dd76c937ba210f3d259b1825670d77939329a3
Summid <summid@foxmail.com>
2023-11-24 16:29:54 +0800

    add Awaitable.AsSTask()

---
+++ Runtime/AwaitableExtensions.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_2023_1_OR_NEWER
+
+namespace SFramework.Threading.Tasks
+{
+    public static class AwaitableExtensions
+    {
+        public static async STask AsSTask(this UnityEngine.Awaitable awaitable)
+        {
+            await awaitable;
+        }
+
+        public static async STask<T> AsSTask<T>(this UnityEngine.Awaitable<T> awaitable)
+        {
+            return await awaitable;
+        }
+    }
+}
+#endif
